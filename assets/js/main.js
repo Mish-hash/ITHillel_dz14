2fb6df2b5cc122d1fa59919686d204372509609b
mish-hash <poluektov23@gmail.com>
2022-08-03 19:49:20 +0300

    1. Greetin after 3 seconds< using promise

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,15 @@
+/*
+1) Напишите код с ипользованим Промиса, который будет вывожить в консоль текст приветствия я через 3 секунды.
+*/
 
+const greetingDelay = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Hello after 3 seconds, used by promise');
+    }, 3000);
+});
 
+greetingDelay.then((greeting) => {
+    console.log(greeting);
+});
 
 
