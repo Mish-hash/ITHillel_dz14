947ace739407381cddfc251b63f3de20942c9e3a
mish-hash <poluektov23@gmail.com>
2022-08-03 20:02:05 +0300

    2. Random number with promise->resolve->reject

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,26 @@ const greetingDelay = new Promise((resolve, reject) => {
 
 greetingDelay.then((greeting) => {
     console.log(greeting);
+    console.log('============================================');
 });
 
+
+/*
+2) Напишите промис внутри которого будет получатся случайное число от 0 - 10,
+если число четное то выполнять resolve, если нет то reject.
+Результат выводить через then & catchю
+*/
+
+const randomNumberPromise = new Promise((resolve, reject)=>{
+    const randomNumber = Math.floor(Math.random() * 10);
+    (randomNumber % 2 === 0) ? resolve(randomNumber) : reject(randomNumber);
+});
+
+randomNumberPromise.then((number)=>{
+    console.log('resolve number ===> ', number);
+}).catch((number)=>{
+    console.log('reject number ===> ', number);
+}).finally(()=>{
+    console.log('============================================');
+});
 
